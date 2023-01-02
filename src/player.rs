2ab4b64961a49721a76479cd26196085d1d44817
Purrie <purriestarshine@gmail.com>
2023-01-02 22:37:11 +0100

    Moved player to the left side of the screen

--- src/player.rs
+++ src/player.rs
@@ -7,6 +7,7 @@ pub struct PlayerPlugin;
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         app.add_startup_system(make_player_sprite)
+            .add_system(player_on_side)
             .add_system(jump_system);
     }
 }
@@ -36,4 +37,10 @@ fn make_player_sprite(mut commands: Commands, _asset_server: Res<AssetServer>) {
         VerticalMove { speed: 1. },
         Player,
     ));
+}
+
+fn player_on_side(mut player: Query<&mut Transform, With<Player>>, camera_view: Query<&OrthographicProjection>) {
+    let mut player = player.get_single_mut().unwrap();
+    let cam = camera_view.get_single().unwrap();
+    player.translation.x = cam.left + 256.;
 }

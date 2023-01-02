845fd4016c0cb322abc55159a82061d3e56be191
Purrie <purriestarshine@gmail.com>
2023-01-02 22:46:07 +0100

    Added bird animations and random spawning
    Birds spawn at random height now.
    Sorted systems into system sets to ensure the game doesn't crash while
    performing operations on a deleted bird, seems like that still happens
    even if I guard it with a proper function call.

--- src/obstacles.rs
+++ src/obstacles.rs
@@ -9,18 +9,35 @@ impl Plugin for ObstaclesPlugin {
         let birb_time = BirdSpawnTimer {
             timer: Timer::new(Duration::new(2, 0), TimerMode::Repeating),
         };
+        let sys = SystemSet::new()
+            .before(Cleanup)
+            .with_system(spawn_birds)
+            .with_system(move_obstacles)
+            .with_system(bird_animation);
+
+        let cleanup = SystemSet::new()
+            .label(Cleanup)
+            .with_system(remove_obstacle);
 
         app.insert_resource(birb_time)
-            // .insert_resource(BirdSprites::default())
-            // .add_startup_system(load_birds)
-            .add_system(spawn_birds)
-            .add_system(move_obstacles)
-            .add_system(remove_obstacle);
+            .insert_resource(BirdSprites::default())
+            .add_startup_system(load_birds)
+            .add_system_set(sys)
+            .add_system_set(cleanup);
     }
 }
 
+/// Label to sort order of system execution
+///
+/// Ensures the other systems don't work on deleted entities
+#[derive(SystemLabel)]
+struct Cleanup;
+
 #[derive(Component)]
 struct Obstacle;
+
+#[derive(Component)]
+struct Bird;
 
 #[derive(Component)]
 struct HorizontalMove {
@@ -32,36 +49,41 @@ struct BirdSpawnTimer {
     timer: Timer,
 }
 
-// #[derive(Resource, Default)]
-// struct BirdSprites {
-//     first: Handle<Image>,
-// }
+#[derive(Resource, Default)]
+struct BirdSprites {
+    first: Handle<Image>,
+    second: Handle<Image>,
+}
 
-// fn load_birds(mut bs: ResMut<BirdSprites>, asset_server: Res<AssetServer>) {
-//     bs.first = asset_server.load("sprites/testchar.png");
-// }
+fn load_birds(mut bs: ResMut<BirdSprites>, asset_server: Res<AssetServer>) {
+    bs.first = asset_server.load("sprites/bird-fly-1.png");
+    bs.second = asset_server.load("sprites/bird-fly-2.png");
+}
 
 fn spawn_birds(
     mut cmd: Commands,
-    // sprites: Res<BirdSprites>,
+    sprites: Res<BirdSprites>,
     mut timer: ResMut<BirdSpawnTimer>,
     time: Res<Time>,
     camera: Query<&OrthographicProjection>,
 ) {
     if timer.tick(time.delta()).just_finished() {
         let op = camera.get_single().unwrap();
-        // let img = sprites.first.clone();
+        let rand_height: f32 = rand::random::<f32>();
+        let height = (rand_height * 0.6) + 0.2;
+        let height = op.top * (1. - height) + op.bottom * height;
+        let img = sprites.first.clone();
         let sprite = (
             SpriteBundle {
-                // texture: img,
+                texture: img,
                 sprite: Sprite {
-                    custom_size: Some(Vec2 { x: 64., y: 64. }),
+                    custom_size: Some(Vec2 { x: 128., y: 128. }),
                     ..Default::default()
                 },
                 transform: Transform {
                     translation: Vec3 {
                         x: op.right + 64.,
-                        y: (op.top + op.bottom) / 2.,
+                        y: height,
                         z: 0.,
                     },
                     ..Default::default()
@@ -69,6 +91,7 @@ fn spawn_birds(
                 ..Default::default()
             },
             Obstacle,
+            Bird,
             HorizontalMove { speed: 500. },
         );
         cmd.spawn(sprite);
@@ -93,4 +116,25 @@ fn remove_obstacle(
         .iter()
         .filter(|x| x.1.translation.x < (op.left - 64.))
         .for_each(|x| cmd.entity(x.0).despawn());
+}
+
+fn bird_animation(
+    mut cmd: Commands,
+    sprites: Res<BirdSprites>,
+    birds: Query<(Entity, &Transform), With<Bird>>,
+) {
+    birds.for_each(|x| {
+        let Some(mut cmd) = cmd.get_entity(x.0) else {
+            return;
+        };
+        let tick = x.1.translation.x.abs() as i32 % 200;
+        match tick >= 100 {
+            true => {
+                cmd.insert(sprites.first.clone());
+            }
+            false => {
+                cmd.insert(sprites.second.clone());
+            }
+        }
+    })
 }
